d7312d3385d5de892b3163ed637759b89ee7e5b8
Gabe Scoggin <gabescoggin@gmail.com>
2018-09-06 21:54:24 -0700

    Update schema.sql

--- models/schema.sql
+++ models/schema.sql
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-DROP DATABASE IF EXISTS books_b;
-CREATE DATABASE books_db;
-
-DROP DATABASE IF EXISTS users_db;
-CREATE DATABASE users_db;
-=======
 DROP DATABASE IF EXISTS biblioteca_development;
 CREATE DATABASE biblioteca_development;
 
 DROP DATABASE IF EXISTS biblioteca_test;
 CREATE DATABASE biblioteca_test;
->>>>>>> 226292d4bc61424f2b85addd2689581adc70f692
+
