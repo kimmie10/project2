7297ce80d1b16f283bc13635d317a667fbcf05e5
Carmen Diaz <cdechauri@gmail.com>
2018-09-14 23:40:41 -0700

    Cleaning stuff

--- models/schema.sql
+++ models/schema.sql
@@ -4,3 +4,5 @@ CREATE DATABASE biblioteca_development;
 DROP DATABASE IF EXISTS biblioteca_test;
 CREATE DATABASE biblioteca_test;
 
+DROP DATABASE IF EXISTS biblioteca_production;
+CREATE DATABASE biblioteca_production;
